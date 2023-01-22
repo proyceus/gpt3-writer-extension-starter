50a7b10174bb1d7ec37caa00574270b5c943b52f
proyceus <proyceus@gmail.com>
2023-01-22 11:51:51 -0500

    coded saveKey to save the openai key to browser

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const saveKey = () => {
+  const input = document.getElementById("key_input");
+
+  if (input) {
+    const { value } = input;
+
+    //encode string
+    const encodeValue = encode(value);
+
+    //save to google storage
+    chrome.storage.local.set({ "openai-key": encodedValue }, () => {
+      document.getElementById("key_needed").style.display = "none";
+      document.getElementById("key_entered").style.display = "block";
+    });
+  }
+};
+
+const changeKey = () => {};
+
 document.getElementById("save_key_button").addEventListener("click", saveKey);
 document
   .getElementById("change_key_button")
